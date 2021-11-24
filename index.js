b452c93225ea817b7f1cac111d29073f7ef13cb4
SrArmas <ricardoarmas77@icloud.com>
2021-11-24 12:16:03 +0100

    first steps wit Git

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(
+  "Don't be afraid to ask questions. Don't be afraid to ask for help when you need it. I do that every day. Asking for help isn't a sign of weakness, it's a sign of strength. It shows you have the courage to admit when you don't know something, and to learn something new. 'Barack Obama'"
+);
